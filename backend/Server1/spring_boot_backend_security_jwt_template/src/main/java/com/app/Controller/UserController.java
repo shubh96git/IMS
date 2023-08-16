caead234211b25f86e9814a9a16573ed5243ee82
shubh96git <shubhamtidke96@gmail.com>
2023-08-16 17:32:42 +0530

    changed name

--- backend/Server1/spring_boot_backend_security_jwt_template/src/main/java/com/app/Controller/UserController.java
+++ backend/Server1/spring_boot_backend_security_jwt_template/src/main/java/com/app/Controller/UserController.java
@@ -5,12 +5,4 @@ import org.springframework.web.bind.annotation.*;
 //
 @RestController
 @RequestMapping("/user")
-public class UserController {
-	
-	// dependancies
-	
-	// Registering the user.
-	
-	
-
-}
+public class UserController {}
