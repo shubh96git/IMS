f67becfd8a15ad54aa6624967594cc9f35da9338
Chaitanya <chintuborakhade@gmail.com>
2023-08-19 08:39:19 +0530

    index file updated

--- backend/NodeSever/index.js
+++ backend/NodeSever/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const credRelatedRoutes= require('./routes/suppliers');
-//const credRelatedRoutes= require('./routes/supplier_addrs');
+const credRelatedRoutes1= require('./routes/suppliers');
+const credRelatedRoutes2= require('./routes/supplier_addrs');
 const app= express();
 app.use((request,response,next)=>{
     response.setHeader('Access-Control-Allow-Origin',"*");
@@ -9,7 +9,7 @@ app.use((request,response,next)=>{
     next();
 })
 app.use(express.json());
-//app.use('/supplier_addrs',credRelatedRoutes)
-app.use('/suppliers',credRelatedRoutes)
+app.use('/supplier_addrs',credRelatedRoutes1)
+app.use('/suppliers',credRelatedRoutes2)
 app.listen(9999,()=>{console.log("Server Started @9999")
 })
