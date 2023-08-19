830e81d1d540b264d2195fcd5229c12f7fdff623
Chaitanya <chintuborakhade@gmail.com>
2023-08-19 08:14:04 +0530

    Supplier Address SHOW END Point

---
+++ backend/NodeSever/routes/supplier_addrs.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const appForClass= express.Router();
+const mysql = require('mysql');
+var connection =mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'manager',
+    database:'inventory_mgmt_system'
+});
+appForClass.get("/",(request,response)=>{
+    connection.query("select * from supplier_addrs",(error,result)=>{
+        if(error==null)
+        {
+            var data = JSON.stringify(result)
+            response.setHeader("Content-Type","application/json");
+            response.write(data);
+        }
+        else{
+            console.log(error);
+            response.setHeader("Content-Type","application/json");
+            response.write(error);
+        }
+        response.end();
+    })
+})
+module.exports=appForClass;
