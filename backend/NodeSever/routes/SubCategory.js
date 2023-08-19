642fc336d798d52554543b0a955a846fe43f2933
AniketPawase <aniket@gmal.com>
2023-08-19 11:40:12 +0530

    EndPoint for AllSubCategory List and ListByid

--- backend/NodeSever/routes/SubCategory.js
+++ backend/NodeSever/routes/SubCategory.js
@@ -37,7 +37,48 @@ appForsubcategory.post("/:id",(request,response) =>{
     })
 })
 
+	//EndPoint to get a list of All SUB Categories
+	//GET:localhost:5000/subcatgry
+	appForsubcategory.get("/",(request,response) =>{
+	connection.query("select * from sub_categories",(error,result) =>
+	{
+	if(error==null)
+	{
+	var data = JSON.stringify(result)
+	response.setHeader("Content-Type","application/json");
+	response.write(data);
+	}
+	else
+	{
+	console.log(error);
+	response.setHeader("Content-Type","application/json");
+	response.write(error);
+	}
+	response.end();
+	})
+	})
 
+//EndPoint to get a list of All SUBCategories  where categoryID supplied
+//GET:localhost:5000/subcatgry/2
+appForsubcategory.get("/:id",(request,response) =>{
+    var query = `select * from sub_categories where category_id=${request.params.id}`;
+    connection.query(query,(error,result) =>
+    {
+    if(error==null)
+    {
+        var data = JSON.stringify(result)
+        response.setHeader("Content-Type","application/json");
+        response.write(data);
+    }
+    else
+    {
+        console.log(error);
+        response.setHeader("Content-Type","application/json");
+        response.write(error);
+    }
+    response.end();
+    })
+})
 
 
 module.exports = appForsubcategory;
