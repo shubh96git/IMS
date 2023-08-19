e8cd48e499e7ff33b5ce414e50c65d32eec477b6
Chaitanya <chintuborakhade@gmail.com>
2023-08-19 08:17:01 +0530

    Supplier Address ADD Endpoint

--- backend/NodeSever/routes/supplier_addrs.js
+++ backend/NodeSever/routes/supplier_addrs.js
@@ -23,4 +23,23 @@ appForClass.get("/",(request,response)=>{
         response.end();
     })
 })
+    appForClass.post("/",(request,response)=>{
+        var query=`insert into supplier_addrs values(${request.body.id},'${request.body.adr_line1}','${request.body.adr_line2}','${request.body.city}','${request.body.country}',
+        '${request.body.state}','${request.body.zip_code}',${request.body.supplier_id})`;
+        connection.query(query,(error,result)=>{
+            if(error==null)
+            {
+                var data =JSON.stringify(result)
+                response.setHeader("Content-type","application/json");
+                response.write(data);
+            }
+            else{
+                console.log(error);
+                response.setHeader("Content-Type","application/json");
+                response.write(error);
+            }
+            response.end();
+     })
+
+ })
 module.exports=appForClass;
